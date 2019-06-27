d6affcf6f5848d3caa8354e85ddb32960888b145
ronyortizG <50431697+ronyortizG@users.noreply.github.com>
2019-06-27 15:36:16 -0400

    Update keygenerator.js

--- src/keygenerator.js
+++ src/keygenerator.js
@@ -1,16 +1,13 @@
 const EC = require('elliptic').ec;
 
-// You can use any elliptic curve you want
 const ec = new EC('secp256k1');
 
-// Generate a new key pair and convert them to hex-strings
 const key = ec.genKeyPair();
 const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
 
-// Print the keys to the console
 console.log();
-console.log('Your public key (also your wallet address, freely shareable)\n', publicKey);
+console.log('Su clave pública (también su dirección de billetera, que se puede compartir libremente)\n', publicKey);
 
 console.log();
-console.log('Your private key (keep this secret! To sign transactions)\n', privateKey);
+console.log('Su clave privada (mantenga este secreto! Para firmar transacciones)\n', privateKey);
